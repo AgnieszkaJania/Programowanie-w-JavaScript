aa864d4ff145a6fa46cb8c19d148b21be2251463
AgnieszkaJania <65187859+AgnieszkaJania@users.noreply.github.com>
2020-12-10 16:41:55 +0100

    Pogoda

---
+++ lab8/main.js
@@ -0,0 +1,41 @@
+class DownloadWeather{
+    constructor(){
+        this.server = 'api.openweathermap.org/data/2.5/weather?q={City}&appid={Key}';
+        this.key = 'dcd1bd22b28d38df327c7af10fe8e896';
+    }
+
+    StringConventer(string, object) {
+        for (const key in object) {
+            const value = object[key];
+
+            string = string.replace('{' + key + '}', value);
+        }
+        return string;
+    }
+
+    GetWeather(name) {
+        const URL =
+            this.StringConventer(this.server, { City: name, Key: this.key });
+
+        const promise = fetch(URL);
+        return promise
+            .then((e) => e.json())
+            .catch(e => console.error(e));
+    }
+
+    
+}
+
+const weatherDownload = new WeatherDownload();
+
+class EventHandlers(){
+
+    constructor(){
+        this.button = document.querySelector('#button');
+        this.inputCity = document.querySelector('#city');
+        this.container = document.querySelector('#container');
+        this.button.addEventListener('click', function(){
+            const cityName = this.inputCity.value;
+        })
+    }
+}
